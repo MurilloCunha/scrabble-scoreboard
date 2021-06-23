978de265b356b3349e58eb04fd1fb035f7834ee2
MurilloCunha <jcunhamurillo@gmail.com>
2021-06-23 14:11:45 -0300

    info-container component

---
+++ src/components/info-container/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function InfoContainer(props) {
+    return (
+        <div class="info-container">
+            {props.children}
+        </div>
+    )
+}
+
+export default InfoContainer
