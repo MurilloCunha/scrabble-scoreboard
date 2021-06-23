9e71590cee4999b0bbf53d8a10d55f399c29e7ab
MurilloCunha <jcunhamurillo@gmail.com>
2021-06-23 12:00:35 -0300

    add logo component

---
+++ src/components/logo/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Logo() {
+    return (
+    <section class="logo">
+        <img class="logo__image"src="./Scrabble-logo.png" alt="scrabble logo" />
+        <h1 class="logo__title">
+            <span class="logo__title--tile">S</span>
+            <span class="logo__title--tile">C</span>
+            <span class="logo__title--tile">O</span>
+            <span class="logo__title--tile">R</span>
+            <span class="logo__title--tile">E</span>
+            <span class="logo__title--tile">B</span>
+            <span class="logo__title--tile">O</span>
+            <span class="logo__title--tile">A</span>
+            <span class="logo__title--tile">R</span>
+            <span class="logo__title--tile">D</span>
+        </h1>
+    </section> 
+    )
+}
+
+export default Logo
