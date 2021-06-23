aa5a22750e597dd3cef35b24808e32038f8d80cd
MurilloCunha <jcunhamurillo@gmail.com>
2021-06-23 11:12:39 -0300

    app models

---
+++ src/models/playersList.js/playersList.js
@@ -0,0 +1,31 @@
+import { player } from "../player/player.js"
+
+export class playersList{
+    constructor(){
+        this._playersList = [new player(1,''),new player(2,'')]
+    }
+
+    get all(){
+        return this._playersList
+    }
+
+    add({id,name}){
+        this._playersList.push(new player(id,name))
+    }
+
+    remove(playerID){
+        this._playersList.splice(playerID,1)
+    }
+
+    updatePlayerName(updateID,value){
+        this._playersList[updateID].setName(value)
+    }
+
+    addPlayerScore(updateID,value){
+        this._playersList[updateID].addScore(value)
+    }
+
+    removePlayerScore(updateID,scoreID){
+        this._playersList[updateID].removeScore(scoreID)
+    }
+}
