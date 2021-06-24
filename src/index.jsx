a4c7688201219d56f076bc3f8b5886dfcd589fd9
MurilloCunha <jcunhamurillo@gmail.com>
2021-06-23 21:28:16 -0300

    apply logo for all pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-
+import Logo from './components/logo/'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Logo />
   </React.StrictMode>,
   document.getElementById('root')
 );
