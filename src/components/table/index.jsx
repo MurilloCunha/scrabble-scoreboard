4cabe304bfe325be06f16458a1ad73d753e913ba
MurilloCunha <jcunhamurillo@gmail.com>
2021-06-24 11:49:55 -0300

    key and className fix

--- src/components/table/index.jsx
+++ src/components/table/index.jsx
@@ -3,21 +3,21 @@ import ScoreInput from '../score-input'
 
 function Table({ playersList }) {
   return (
-    <table class="scoreboard__table">
+    <table className="scoreboard__table">
       <thead>
         <tr>
-          {playersList.map(player => <th>{player.name}</th>)}
+          {playersList.map(player => <th key={`header_${player.name}`}>{player.name}</th>)}
         </tr>
       </thead>
       <tbody>
         {playersList[0].score.map((scoreRow, scoreRowIndex) =>
-          <tr>{playersList.map(player => <td>{player.score[scoreRowIndex] || <ScoreInput player />}</td>)}</tr>
+          <tr>{playersList.map(player => <td key={`${player.name}_${scoreRowIndex}`}>{player.score[scoreRowIndex] || <ScoreInput player />}</td>)}</tr>
         )}
         <tr>
-          {playersList.map(player => <td>{<ScoreInput player={player} />}</td>)}
+          {playersList.map(player => <td key={`scoreInput_${player.name}`}>{<ScoreInput player={player} />}</td>)}
         </tr>
         <tr>
-          {playersList.map(player => <td class="scoreboard__table-total">{player.totalScore}</td>)}
+          {playersList.map(player => <td key={`total_${player.name}`} className="scoreboard__table-total">{player.totalScore}</td>)}
         </tr>
       </tbody>
     </table>
