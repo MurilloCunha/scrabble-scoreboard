82328f119a42e125909afa3adace0f6c9bd40a7b
MurilloCunha <jcunhamurillo@gmail.com>
2021-06-24 19:01:23 -0300

    scoreboard layout update  add end button

--- src/views/scoreboard/index.jsx
+++ src/views/scoreboard/index.jsx
@@ -3,14 +3,16 @@ import React, { useContext } from 'react'
 import Match from '../../context/status'
 import Table from '../../components/table/'
 import SearchInput from '../../components/search-input/'
+import Button from '../../components/button/'
 
 function Scoreboard() {
   const matchPlayers = useContext(Match)
 
   return (
     <section className='scoreboard'>
-      <Table playersList={matchPlayers.all} />
       <SearchInput />
+      <Table playersList={matchPlayers.all} />
+      <Button>Finalizar</Button>
     </section>
 
   )
