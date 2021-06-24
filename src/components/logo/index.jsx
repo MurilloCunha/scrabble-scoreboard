c878a9f4f0a778eae387ed045cf4beefa999ed4f
MurilloCunha <jcunhamurillo@gmail.com>
2021-06-23 21:04:13 -0300

    className fix

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 function Logo() {
     return (
-    <section class="logo">
-        <img class="logo__image"src="./Scrabble-logo.png" alt="scrabble logo" />
-        <h1 class="logo__title">
-            <span class="logo__title--tile">S</span>
-            <span class="logo__title--tile">C</span>
-            <span class="logo__title--tile">O</span>
-            <span class="logo__title--tile">R</span>
-            <span class="logo__title--tile">E</span>
-            <span class="logo__title--tile">B</span>
-            <span class="logo__title--tile">O</span>
-            <span class="logo__title--tile">A</span>
-            <span class="logo__title--tile">R</span>
-            <span class="logo__title--tile">D</span>
+    <section className="logo">
+        <img className="logo__image"src="./Scrabble-logo.png" alt="scrabble logo" />
+        <h1 className="logo__title">
+            <span className="logo__title--tile">S</span>
+            <span className="logo__title--tile">C</span>
+            <span className="logo__title--tile">O</span>
+            <span className="logo__title--tile">R</span>
+            <span className="logo__title--tile">E</span>
+            <span className="logo__title--tile">B</span>
+            <span className="logo__title--tile">O</span>
+            <span className="logo__title--tile">A</span>
+            <span className="logo__title--tile">R</span>
+            <span className="logo__title--tile">D</span>
         </h1>
     </section> 
     )
