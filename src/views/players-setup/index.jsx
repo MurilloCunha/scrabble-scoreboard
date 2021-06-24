946d65c10e6d59025c4c77f920ea28d5c524c176
MurilloCunha <jcunhamurillo@gmail.com>
2021-06-24 10:41:43 -0300

    consume playersList from global context

--- src/views/players-setup/index.jsx
+++ src/views/players-setup/index.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import Match from '../../context/status'
 import PlayerInput from '../../components/player-input/'
 import Button from '../../components/button/'
 
-function PlayersSteup({playersList}) {
-    return (
+function PlayersSteup() {
+  return (
+    <Match.Consumer>
+      {(matchPlayers) => (
         <section className="players-setup">
-            {playersList.map(player => <PlayerInput player={player}/>)}
-            <Button variant="ghost">+</Button>
-            <Link to="/scoreboard">
-                <Button>Começar</Button>
-            </Link>
+          {matchPlayers.all.map(player => <PlayerInput player={player} />)}
+          <Button variant="ghost">+</Button>
+          <Link to="/scoreboard">
+            <Button>Começar</Button>
+          </Link>
         </section>
-    )
+      )}
+    </Match.Consumer>
+  )
 }
 
 export default PlayersSteup
--- src/views/scoreboard/index.jsx
+++ src/views/scoreboard/index.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+
+import Match from '../../context/status'
 import Table from '../../components/table/'
 import SearchInput from '../../components/search-input/'
 
-function Scoreboard({playersList}) {
-    return (
+function Scoreboard() {
+  return (
+    <Match.Consumer>
+      {(matchPlayers) => (
         <section className='scoreboard'>
-            <Table playersList={playersList}/>
-            <SearchInput />
+          <Table playersList={matchPlayers.all} />
+          <SearchInput />
         </section>
-        
-    )
+      )}
+    </Match.Consumer>
+  )
 }
 
 export default Scoreboard
