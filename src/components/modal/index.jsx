9f83174d53cd5aca1bc0b9b4be059f43b2494d1d
MurilloCunha <jcunhamurillo@gmail.com>
2021-07-23 16:55:03 -0300

    modal update

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import Button from '../button'
+
 function Modal(props) {
 
   return (
     <section className="modal">
       <div className="modal__container">
-        <Link to={props.to}><button className="modal__close-button"type="button" {...props}>x</button></Link>
+        <Link to={props.to}><Button className="modal__close-button"type="button" {...props}>x</Button></Link>
         <div className="modal__content">{props.children}</div>
         </div>
     </section>
