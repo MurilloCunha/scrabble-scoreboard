41d98432e00b1dfe5a837a7c8c3045ee67c40fb8
MurilloCunha <jcunhamurillo@gmail.com>
2021-10-05 17:30:35 -0300

    TypeScript install fix

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Logo from './components/logo';
+import Routes from './routes';
+import Footer from './components/footer';
+
+function App() {
+  return (
+   <>
+   <Logo />
+   <Routes />
+   <Footer />
+   </>
+  );
+}
+
+export default App;
