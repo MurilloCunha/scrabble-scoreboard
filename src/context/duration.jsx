46e73b59088b520890dbd9b2a42646294d82c577
MurilloCunha <jcunhamurillo@gmail.com>
2021-10-07 15:43:07 -0300

    fix: remove unused context and models

--- src/context/duration.jsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react'
-import { gameDuration } from '../models/game-duration'
-
-const matchTime = new gameDuration() 
-
-const MatchDuration = createContext(matchTime)
-
-export default MatchDuration
--- src/context/status.jsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react'
-import { playersList } from '../models/playersList/'
-
-const matchPlayers = new playersList()
-
-const Match = createContext(matchPlayers)
-
-export default Match
