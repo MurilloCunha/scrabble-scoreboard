e8ab8779aec4768dc965ee27792ffa6662e69ee6
MurilloCunha <jcunhamurillo@gmail.com>
2021-10-14 18:29:11 -0300

    feat: score list component

---
+++ src/components/scoreboard/scoreboard-column/score-list/score-list.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react'
+
+import ScoreList from './score-list'
+
+
+describe('test score-list', () => {
+  test.each([0,1,2,3,4])('renders player scores', (score) => {
+
+    render(
+    <ScoreList 
+      player={{id:0,name:'', higherScore:0, lowerScore:0,score:[0,1,2,3,4],totalScore:0}}
+      game={{
+        state:{
+          players:[
+            {id:0,name:'', higherScore:0, lowerScore:0,score:[0],totalScore:0}
+          ],
+          stats:{
+            gameDuration:0,
+            higherScore:{player:'',score:0},
+            lowerScore:{player:'',score:0},
+            winner:''
+          }},
+          addPlayer: () => {},
+          addScore: () => {},
+          deleteScore: () => {},
+          startMatch: () => {},
+          endMatch: () => {},
+          
+        }}
+    />)
+    const scoreList = screen.getByRole('list')
+
+    expect(scoreList.children[score].innerHTML).toBe(`${score}`)
+  })
+})
