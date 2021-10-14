42ffda0c81ec6c3eca4cdeac21f4a534cdb72b33
MurilloCunha <jcunhamurillo@gmail.com>
2021-10-14 18:29:43 -0300

    feat: scoreboard component

---
+++ src/components/scoreboard/scoreboard.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react'
+
+import Scoreboard from './scoreboard'
+
+
+describe('test scoreboard', () => {
+  test('renders', () => {
+
+    render(
+    <Scoreboard 
+      game={{
+        state:{
+          players:[
+            {id:0,name:'test', higherScore:0, lowerScore:0,score:[0,1,2,3,4],totalScore:120},
+            {id:1,name:'test', higherScore:0, lowerScore:0,score:[0,1,2,3,4],totalScore:120},
+            {id:2,name:'test', higherScore:0, lowerScore:0,score:[0,1,2,3,4],totalScore:120},
+          ],
+          stats:{
+            gameDuration:0,
+            higherScore:{player:'',score:0},
+            lowerScore:{player:'',score:0},
+            winner:''
+          }},
+          addPlayer: () => {},
+          addScore: () => {},
+          deleteScore: () => {},
+          startMatch: () => {},
+          endMatch: () => {},
+          
+        }}
+    />)
+    const scoreboardColumn = screen.getByTestId('scoreboard')
+    expect(scoreboardColumn.children.length).toBe(3)
+
+  })
+})
---
+++ src/components/scoreboard/scoreboard.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { GameHook } from '../../interfaces'
+import ScoreBoardColumn from './scoreboard-column/scoreboard-column'
+import './scoreboard.style.scss'
+
+interface Props {
+  game: GameHook
+}
+
+function Scoreboard(props: Props) {
+  const { game } = props
+  return (
+    <div className="scoreboard" data-testid="scoreboard">
+      {game.state.players.map(player => (
+        <ScoreBoardColumn key={player.id} player={player} game={game} />
+      ))}
+    </div>
+  )
+}
+
+export default Scoreboard
