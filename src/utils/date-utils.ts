b8bafe00f1bd23b007629b23e700afab264c32dc
MurilloCunha <jcunhamurillo@gmail.com>
2021-10-19 15:13:34 -0300

    feat: transform seconds to hour format.

---
+++ src/utils/date-utils.ts
@@ -0,0 +1,8 @@
+export const secondsToTime = (seconds: number): string => {
+  return new Date(seconds)
+    .toISOString()
+    .substr(11,8)
+    .replace(':','h ')
+    .replace(':','m ')
+    .replace(':','s ')
+}
