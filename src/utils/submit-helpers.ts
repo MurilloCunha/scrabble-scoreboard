eba2c5f2c10978bc556d910ede032f8434af74c9
MurilloCunha <jcunhamurillo@gmail.com>
2021-10-19 17:45:21 -0300

    feat: hasError submit helper

---
+++ src/utils/submit-helpers.ts
@@ -0,0 +1,38 @@
+import { GameHook } from "../interfaces"
+
+export const getFormData = (formElement: HTMLFormElement, names: string[]): {[key: string]: string[]} => {
+  const formData = new FormData(formElement)
+  const formValues = names.reduce((values, name) =>
+   ({ ...values, [name]: formData.getAll(name) }),{}
+  )
+  return formValues
+}
+
+export const hasRepeatedValues = (input :any[]): boolean => {
+  const inputSet = new Set(input)
+
+  return inputSet.size === input.length ? false : true
+}
+
+export const hasEmptyValues = (input :any[]): boolean => {
+  const inputSet = new Set(input)
+
+  return inputSet.has('')
+}
+
+export const hasErrors = (input :any[]): string => {
+  
+  if (hasEmptyValues(input)) {
+    return "Todos os jogadores devem ter nomes."
+    
+  }
+   if (hasRepeatedValues(input)) {
+    return "Não podem existir dois jogadores com o mesmo nome."
+    
+  }
+  return ""
+}
+
+export const registerPlayers = (players: string[], game: GameHook): void => {
+  players.forEach(player => game.addPlayer(player))
+}
