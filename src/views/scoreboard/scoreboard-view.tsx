b6f01a6dd33b2d8e850f30af396a065290d8f89f
MurilloCunha <jcunhamurillo@gmail.com>
2021-10-19 17:44:28 -0300

    feat: scoreboard view styled

--- src/views/scoreboard/scoreboard-view.tsx
+++ src/views/scoreboard/scoreboard-view.tsx
@@ -1,20 +1,19 @@
 import Button from "../../components/button/button";
+import Modal from "../../components/modal/modal";
 import Scoreboard from "../../components/scoreboard/scoreboard";
 import SearchInput from "../../components/search-input/search-input";
 import useDictionary from "../../hooks/use-Dictionary";
 import useNavigation from "../../hooks/use-navigation";
-import { GameHook, wordData } from "../../interfaces";
-
-
+import { GameHook } from "../../interfaces";
 
 function ScoreboardView(game: GameHook) {
-  const dictionary = useDictionary<wordData[]>()
+  const dictionary = useDictionary()
   const navigation = useNavigation()
 
 
   const handleGameEnd = () => {
     game.endMatch()
-    setTimeout(() => navigation.next(),3000)
+    navigation.next()
   }
   
   return (
@@ -22,14 +21,17 @@ function ScoreboardView(game: GameHook) {
       <header>
         <h1>Placar</h1>
       </header>
-      <SearchInput searchFunction={dictionary.get}/>
-      <Scoreboard game={game} />
-      {dictionary.isLoading && <p>Loading</p>}
-      {!dictionary.isLoading && dictionary.response &&
-        <p>{JSON.stringify(dictionary.response,null,2)}</p>
-      }
-  {!dictionary.isLoading && dictionary.error && <p>{dictionary.error}</p>}
-      <Button variant="primary" onClick={handleGameEnd}>Finalizar</Button>
+      <main>
+        <SearchInput searchFunction={dictionary.get} />
+        <Modal show={dictionary.isLoading}>
+          {dictionary.isLoading && <p>Loading</p>}
+          {!dictionary.isLoading && !!dictionary.response && dictionary.response}
+          <p>{dictionary.error?.message}</p>
+        </Modal>
+        <Scoreboard game={game} />
+        <Button variant="primary" onClick={handleGameEnd}>Finalizar</Button>
+      </main>
+      <footer></footer>
     </>
   );
 }
