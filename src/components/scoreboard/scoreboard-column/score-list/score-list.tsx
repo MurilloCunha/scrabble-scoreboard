07c1b14427fa4083b585fd3a1647e2af645c4870
MurilloCunha <jcunhamurillo@gmail.com>
2021-10-20 18:23:26 -0300

    update: style update

--- src/components/scoreboard/scoreboard-column/score-list/score-list.tsx
+++ src/components/scoreboard/scoreboard-column/score-list/score-list.tsx
@@ -1,37 +1,37 @@
-import React, { useState } from 'react'
-import { GameHook, Player } from '../../../../interfaces'
-import Button from '../../../button/button'
-import './score-list.style.scss'
+import React, { useState } from "react"
+import { GameHook, Player } from "../../../../interfaces"
+import Button from "../../../button/button"
+import "./score-list.style.scss"
 
 interface Props {
   player: Player
   game: GameHook
 }
 
-function ScoreList(props: Props) {
-  const [inputOptions, setInputOptions] = useState({index: 0, show:false})
-  const { player, game } = props
+function ScoreList(props: Props): JSX.Element {
+	const [inputOptions, setInputOptions] = useState({index: 0, show:false})
+	const { player, game } = props
 
-  const handleOptions = (index: number): void =>{
-    setInputOptions({ index, show:!inputOptions.show})
-  }
+	const handleOptions = (index: number): void =>{
+		setInputOptions({ index, show:!inputOptions.show})
+	}
 
-  const handleDeleteScore = (index:number) => {
-    game.deleteScore(player.id, index)
-  }
+	const handleDeleteScore = (index:number) => {
+		game.deleteScore(player.id, index)
+	}
 
-  return (
-      <ul className="score-list">
-        {player.score.map((score, index) => (
-          <li className="score-list__score" onClick={() => handleOptions(index)} key={`${player.id}_score${index}`}>
-            {score}
-            {inputOptions.index === index && inputOptions.show &&
+	return (
+		<ul className="score-list">
+			{player.score.map((score, index) => (
+				<li className="score-list__score" onClick={() => handleOptions(index)} key={`${player.id}_score${index}`}>
+					{score}
+					{inputOptions.index === index && inputOptions.show &&
              <Button onClick={() => handleDeleteScore(index)} variant="delete">X</Button>
-            }
-          </li>
-        ))}
-      </ul>
-  )
+					}
+				</li>
+			))}
+		</ul>
+	)
 }
 
 export default ScoreList
